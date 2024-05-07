c5ecac507bf5a0aa19667a1bb939e58d41355fb2
Daniel <roderm99@gmail.com>
2024-05-07 12:44:00 -0400

    add 'resume builder' title to page; use media query to remove it from print

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { ContactForm, EduForm, WorkForm } from './components/Form.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h2>RESUME BUILDER</h2>
     <ContactForm name='' email='' cell='' />
     <EduForm school='' degree='' gradDate='' additionalInfo='' />
     <WorkForm company='' position='' dateStart='' dateEnd='' details='' />
